0949e18282558b36aab74f4c99485c0deac9a310
Juan Pablo Prado <github@juanpabloprado.com>
2024-12-03 19:41:23 -0600

    Update 01-download-kafka.sh

--- 01-download-kafka.sh
+++ 01-download-kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o kafka.tgz -L https://downloads.apache.org/kafka/3.7.1/kafka_2.13-3.7.1.tgz
+curl -o kafka.tgz -L https://downloads.apache.org/kafka/3.9.0/kafka_2.13-3.9.0.tgz
 tar -xf kafka.tgz
 mv kafka_2.13-* kafka_home
 rm kafka.tgz
